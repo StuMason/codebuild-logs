7d0397a0598099564d6da25da9baa2264983620b
Stu Mason <stu@stuartmason.co.uk>
2021-03-29 18:06:28 +0100

    starting

--- main.py
+++ main.py
@@ -1,13 +1,49 @@
 import os
-import requests  # noqa We are just importing this to prove the dependency installed correctly
+import boto3
+import sys
+from datetime import datetime
 
 
 def main():
-    my_input = os.environ["INPUT_MYINPUT"]
+    key = os.environ["AWS_KEY"]
+    secret = os.environ["AWS_SECRET"]
+    region = os.environ["AWS_REGION"]
+    project = os.environ["CODEBUILD_PROJET_NAME"]
 
-    my_output = f"Hello {my_input}"
+    codebuild = boto3.client(
+        'codebuild', 
+        region_name=region,
+        aws_access_key_id=key,
+        aws_secret_access_key=secret,
+    )
 
-    print(f"::set-output name=myOutput::{my_output}")
+    response = codebuild.list_builds_for_project(
+        projectName=project,
+        sortOrder='DESCENDING'
+    )
+
+    log_stream_name = response['ids'][0].split(":")[1]
+
+    logs = boto3.client(
+        'logs',
+        region_name=region,
+        aws_access_key_id=key,
+        aws_secret_access_key=secret,
+    )
+
+    response = logs.get_log_events(
+        logGroupName=f'/aws/codebuild/{project}',
+        logStreamName=log_stream_name,
+        startFromHead=True
+    )
+
+    for log in response['events']:
+        time = datetime.fromtimestamp(log['timestamp'] / 1000)
+        event = log['message']
+        if len(event.strip()) > 1:
+            print(f'{time.strftime("%Y-%m-%d %H:%M:%S")}: {event.strip()}')
+
+    sys.exit(0)
 
 
 if __name__ == "__main__":
